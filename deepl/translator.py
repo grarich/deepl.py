7387412da38574a8cd3c6fa29512ac340c93cae5
grarich123 <grarich123+github@gmail.com>
2021-05-03 01:31:26 +0900

    frat: Create a class to call from Python.

---
+++ deepl/translator.py
@@ -0,0 +1,26 @@
+from .adapter import Adapter
+
+
+class Translator:
+    def __init__(self, adapter: Adapter):
+        self._adapter = adapter
+
+    def translate(self, text, target_lang, *, source_lang=None, split_sentences=None, preserve_formatting=None, formality=None):
+        payload = {
+            'text': text,
+            'target_lang': str(target_lang)
+        }
+        if source_lang:
+            payload['source_lang'] = source_lang
+        if split_sentences:
+            payload['split_sentences'] = preserve_formatting
+        if formality:
+            payload['formality'] = formality
+        return self._adapter.get_translated_text(payload)
+
+    def usage(self):
+        return self._adapter.get_usage()
+
+    def supported_languages(self):
+        return self._adapter.get_supported_languages()
+
