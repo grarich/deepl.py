c23c14e692b1c458f83042a2529aed059c4eb39d
grarich123 <grarich123+github@gmail.com>
2021-05-03 01:38:00 +0900

    feat: Create setup.py to install it as a library.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='deepl.py',
+    version='0.0.1',
+    author='grarich',
+    author_email='grarich123@gmail.com',
+    description='A Python wrapper for the DeepL API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/grarich123/deepl.py',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
