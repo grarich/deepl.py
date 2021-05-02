9e140dd0636fa7275ef605eba2fe60da1639a304
grarich123 <grarich123+github@gmail.com>
2021-05-03 01:32:34 +0900

    frat: Add __init__.py so that it can be imported as a library.

---
+++ deepl/__init__.py
@@ -0,0 +1,19 @@
+"""
+DeepL API Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the DeepL API.
+:copyright: (c) 2021-present grarich
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'deepl'
+__author__ = 'grarich'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present grarich'
+__version__ = '0.0.1'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from .adapter import RequestsAdapter, AiohttpAdapter
+from .enums import *
+from .translator import Translator
